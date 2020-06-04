0e8ad02e31a4136ee5151c9de4c3b60c16c715b2
Jack <jackrrye@gmail.com>
2020-06-04 17:58:56 -0500

    all the index js methods are planned out, just not written AND the pass along is working to renderPlayer

--- index.js
+++ index.js
@@ -10,12 +10,43 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch(PLAYERS_URL)
         .then(response => response.json())
         .then(players => {
-            // renderPlayers()
+            renderPlayers(players.data)
         })
     }
 
 )
 
 function renderPlayers(playersData) {
+    // console.log(playersData)
+    playersData.forEach(player => {
+        // console.log("rendering a player")
+        renderPlayer(player);
+    });
+}
+
+// have a release button for each pokemon
+// let releaseButton = document.createElement("button");
+// releaseButton.innerHTML = "Release"
+// releaseButton.classList.add("release")
+// releaseButton.setAttribute("data-pokemon-id", `${pokemon.id}`)
+
+function renderPlayer(playerData) {
+    console.log(playerData)
     
+    //create payer div and add to the DOM
+    //add name (h2) to the div
+    //render characters
+}
+
+function renderCharacters(charactersData) {
+    charactersData.forEach(character => {
+        renderCharacter(character)
+    })
+}
+
+function renderCharacter(characterData) {
+    //create a character div
+    //add name (h3) to the div
+    //create a 3x4 table
+    //populate the table with the character data
 }
