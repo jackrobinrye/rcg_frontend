05720d65b33e75fea2b734a598e4e8eb2e045f98
Jack <jackrrye@gmail.com>
2020-06-04 15:53:46 -0500

    trying to get my fetch request to players_url working

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("in index.js")
+PLAYERS_URL = "http://localhost:3000/api/players"
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("loaded")
+
+    fetch(PLAYERS_URL, {mode: 'no-cors'})
+    .then(function(response) {
+        console.log(response); 
+        console.log(response.json()); 
+        console.log("not the catch")
+    }).catch(function(error) {  
+        console.log('Request failed', error)  
+    });
+
+    // function fetchPlayers() {
+    // fetch(PLAYERS_URL, {mode: 'no-cors'})
+    //     .then(response => response.json())
+    //     .then(players => {
+    //         console.log(players);
+    //     })
+    // }
+    // fetchPlayers()
+})
