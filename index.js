e2b44ad7fcd29ad9f6e153bd5039278c7bf7cb94
Jack <jackrrye@gmail.com>
2020-06-04 17:32:41 -0500

    got the fetch to successfully fetch my data

--- index.js
+++ index.js
@@ -3,21 +3,19 @@ PLAYERS_URL = "http://localhost:3000/api/players"
 document.addEventListener("DOMContentLoaded", () => {
     console.log("loaded")
 
-    fetch(PLAYERS_URL, {mode: 'no-cors'})
-    .then(function(response) {
-        console.log(response); 
-        console.log(response.json()); 
-        console.log("not the catch")
-    }).catch(function(error) {  
-        console.log('Request failed', error)  
-    });
+    function fetchPlayers() {
+        fetchPlayers()
+    }
 
-    // function fetchPlayers() {
-    // fetch(PLAYERS_URL, {mode: 'no-cors'})
-    //     .then(response => response.json())
-    //     .then(players => {
-    //         console.log(players);
-    //     })
-    // }
-    // fetchPlayers()
-})
+    fetch(PLAYERS_URL)
+        .then(response => response.json())
+        .then(players => {
+            // renderPlayers()
+        })
+    }
+
+)
+
+function renderPlayers(playersData) {
+    
+}
